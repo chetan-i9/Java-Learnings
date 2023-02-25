4af4f92980eedddd0ff17b099681593bff4ed66d
Chetan Umesh Ingole <98551496+chetan-i9@users.noreply.github.com>
2023-02-25 13:30:09 +0530

    Create Prob2.java

---
+++ Module1/Prob2.java
@@ -0,0 +1,12 @@
+	import java.util.*;
+public class Prob2 {
+
+	public static void main(String[] args) {
+		// your code here
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name=scn.nextLine();
+		System.out.println("Hello "+name+"!  Welcome to Event Management System.");
+	}
+
+}
